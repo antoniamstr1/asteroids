e3386dbb0eee1c9dcd914bc7253818ed30fcca96
antoniamstr1 <mestrovic.antonia@gmail.com>
2023-11-15 10:32:12 +0100

    osnovno gotovo + slike

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
+app.use('/images', express.static('images'));
+
 app.set('views', __dirname + '/views');
 app.use(session({
     secret: 'your-secret-key',
